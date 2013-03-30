4911241decd906e9e85545a3207ba4a7f8e472b9
Jakub Filak <jfilak@redhat.com>
2013-03-30 01:58:57 +0100

    dump dirs: add download/delete all buttons
    Signed-off-by: Jakub Filak <jfilak@redhat.com>

--- pyfaf/hub/media/form.js
+++ pyfaf/hub/media/form.js
@@ -37,3 +37,25 @@ $(document).ready(function() {
             second_button.attr('href') + match[2]);
     }
 });
+
+function dumpdiraction(action) {
+	var url = '';
+	$('li.active a').each(function() {
+	  if($(this).attr('href').length > url.length) {
+	    url = $(this).attr('href');
+	  }
+	});
+	var options = $('input:checked').map(function() {
+			  return $(this).attr('value');
+		      }).get().join(',');
+	if (options) {
+		window.location = url + action + '/' + options;
+	}
+	else {
+		alert("Empty selection for action " + action);
+        }
+};
+
+function toggleChecked(selector, checked) {
+	$(selector).each(function(){$(this).attr('checked', checked);});
+};
